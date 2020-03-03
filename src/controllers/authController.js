57ea663aa935a6001d80ab27099424ed99ba0455
Lucas Schroeder <lucas.ems@hotmail.com>
2020-03-03 08:27:47 -0300

    feature token authenticate

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs')
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
 
 const router = express.Router();
 router.post('/register', async (req, res) => {
@@ -20,4 +22,25 @@ router.post('/register', async (req, res) => {
   }
 
 });
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password} = req.body;
+  const user = await User.findOne ({ email }).select('+password');
+
+  if (!user)
+    return res.status(400).send({ error: 'User not found' });
+  if ( !await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Invalid password' });
+  
+  user.password = undefined;
+
+  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+    expiresIn: 86400,
+  })
+
+  res.send({ user, token });
+});
+
+
+
 module.exports = app => app.use('/auth', router);
