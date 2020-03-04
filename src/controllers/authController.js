5bfb5304691b94cedc30ad8b44aa6ec64c1e19c1
Lucas Schroeder <lucas.ems@hotmail.com>
2020-03-03 21:36:12 -0300

    chore: auth functions

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,13 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth');
 
 const router = express.Router();
+
+function generateToken (params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
+
 router.post('/register', async (req, res) => {
   const { email } = req.body;
 
@@ -16,7 +23,11 @@ router.post('/register', async (req, res) => {
 
     user.password = undefined;
 
-    return res.send ({ user });
+    return res.send ({ 
+      user,
+      token: generateToken ({id: user.id}),
+     });
+     
   } catch (err) {
     return res.status(400).send({error: 'Registration failed'});
   }
@@ -34,11 +45,10 @@ router.post('/authenticate', async (req, res) => {
   
   user.password = undefined;
 
-  const token = jwt.sign({ id: user.id }, authConfig.secret, {
-    expiresIn: 86400,
-  })
-
-  res.send({ user, token });
+  res.send({ 
+    user, 
+    token: generateToken ({ id: user.id }),
+  });
 });
 
 
